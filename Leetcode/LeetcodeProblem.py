10fb294f5e6a9fceda3d82470edb90595a9fe76d
William Chiang <williamchiang@gmail.com>
2021-10-24 23:15:21 +0800

    update

--- Leetcode/LeetcodeProblem.py
+++ Leetcode/LeetcodeProblem.py
@@ -367,3 +367,22 @@ class Solution:
                 break
         return 
     
+#977. Squares of a Sorted Array
+# Given an integer array nums sorted in non-decreasing order, 
+#   return an array of the squares of each number sorted in non-decreasing order.
+# Input: nums = [-4,-1,0,3,10]
+# Output: [0,1,9,16,100]
+# Explanation: After squaring, the array becomes [16,1,0,9,100].
+# After sorting, it becomes [0,1,9,16,100].
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        left = 0
+        right = len(nums) - 1
+        opt = [None for num in nums]
+        for idx in range(len(nums)):
+            if abs(nums[left]) > abs(nums[right]):
+                opt[idx] = nums[left] ** 2
+                left += 1
+            else:
+                opt[idx] = nums[right] ** 2
+                right -= 1
+        return opt[::-1]
